576e8d7ea85987cbe5530fe3792e7d50101a6549
Chad <chadlada@gmail.com>
2022-01-02 15:30:55 -0500

    Added ToUpper for user result

--- Program.cs
+++ Program.cs
@@ -14,8 +14,6 @@ namespace NumberGuesser
 
             ShowGreeting();
             GuessNumber(min, max);
-
-
         }
 
 
@@ -24,7 +22,7 @@ namespace NumberGuesser
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("    Welcome to The Number Guesser    ");
             Console.WriteLine("----------------------------------------");
-            Console.WriteLine("I will use my black magic to determine a number between 1 and 1024..Think of it in your head. Don't tell me. then hit enter");
+            Console.WriteLine("I will use my black magic to determine a number between 1 and 1024..Think of it in your head. Don't tell me.\n Hit ENTER when ready");
             Console.WriteLine();
             Console.ReadLine();
         }
@@ -32,31 +30,27 @@ namespace NumberGuesser
         static void GuessNumber(int low, int high)
         {
             var mid = (low + high) / 2;
-            Console.WriteLine($"Is your number {mid}? \nType 'y' for yes, 'l' for lower, or 'h' for higher.");
-            var result = Console.ReadLine();
+            Console.WriteLine($"is {mid} your number? \nType 'Y' for Yes, 'H' for higher, or 'L' for Lower");
+            var result = Console.ReadLine().ToUpper();
 
-            if (result == "y")
+            if (result == "Y")
             {
-                Console.WriteLine("I got it!");
+                Console.WriteLine("I got it! I got it! I read your mind!\n\n");
             }
-            else if (result == "l")
-            {
-                var newMax = mid;
-                // Console.WriteLine("mid " + mid);
-                GuessNumber(low, newMax);
-                // -if lower, then set guessed number as new maximum, keep minimum, and determine new middle for next guess. 
-                // (max512 min0)
-            }
-            else if (result == "h")
+            else if (result == "H")
             {
                 var newMin = mid;
                 GuessNumber(newMin, high);
-                // -if higher, then set guessed number as new minimum, keep maximum, and determine new middle for next guess. 
-                // (Max1024 min512)
             }
+            else if (result == "L")
+            {
+                var newMax = mid;
+                GuessNumber(low, newMax);
+            }
+
             else
             {
-                Console.WriteLine("Please type 'y', 'l', or 'h'");
+                Console.WriteLine("Unexpected response..Cannot compute..");
             }
 
         }
